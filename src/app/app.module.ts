ae72a0599231ad9806a36e91aa8a3fc7ab25fcbd
Emil Verdiyev <verdiyev.emil@gmail.com>
2021-09-21 22:17:28 +0400

    created project layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Material modules
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+// Components
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ListItemComponent,
+    WorkspaceComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
